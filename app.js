8b187a36fd4f3932acf78303a38bda4092e4c789
Roshan Kumar <roshanmaurya729@gmail.com>
2022-08-24 20:54:06 +0530

    Configured file upload using multer

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-// var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session); //takes session as parameter
 const passport = require('passport');
 const authenticate = require('./authenticate');
 const config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -25,7 +25,18 @@ connect.then( (db) => {
   console.log(err);
 });
 
-var app = express();
+const app = express();
+
+// redirecting all the incoming requests to the secure port
+app.all('*', (req, res, next) => {
+  if(req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+  //307 returned statuscode tells user to retry with the same method again
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/uploadRouter.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const authenticate = require('../authenticate');
+const multer = require('multer');
+
+// diskStorage enables us to define the storage engine
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => { //cb=callback
+        cb( null, 'public/images' ); //error=null, path of storage
+    },
+
+    filename: (req, file, cb) => {
+        cb( null, file.originalname); //original name is the name from the client side
+    }
+});
+
+// enables us to specify the kind of files supported for uploading
+const imageFileFilter = (req, file, cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('You can only upload image files!'), false);
+    }
+    else {
+        return cb(null, true); //true= go ahead with upload
+    }
+};
+
+const upload = multer({storage: storage, fileFilter: imageFileFilter});
+//multer module configuration complete
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(express.json());
+
+uploadRouter.route('/')
+.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+
+    res.statusCode = 403;
+    res.end('GET operation not supported on /imageUpload'); 
+
+ })
+ .post(authenticate.verifyUser, authenticate.verifyAdmin,
+    upload.single('imageFile'), (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json')
+    res.json(req.file);
+ })
+ .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /imageUpload'); 
+ })
+ .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /imageUpload'); 
+ })
+
+module.exports = uploadRouter;
